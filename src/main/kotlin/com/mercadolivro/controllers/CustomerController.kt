09226544079aeec5c425051ed5f52f68ebb6fa16
luiz fernando <luiz123jfmg@gmail.com>
2022-01-09 14:26:11 -0300

    Primeiro endpoint =)
    Primeiro endpoint em Kotlin

---
+++ src/main/kotlin/com/mercadolivro/controllers/CustomerController.kt
@@ -0,0 +1,15 @@
+package com.mercadolivro.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("customer")
+class CustomerController {
+
+    @GetMapping
+    fun Hello(): String{
+        return "Primeiro endpoint em kotlin =)"
+    }
+}
