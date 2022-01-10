b7ede0bc11eba5b05c5855c1fec2af6819721f4a
luiz fernando <luiz123jfmg@gmail.com>
2022-01-10 19:11:46 -0300

    [Refactor] PostMapping
    refatorando a rota Post, deixando dinamica
    Criando uma lista e add as entidades

--- src/main/kotlin/com/mercadolivro/controllers/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controllers/CustomerController.kt
@@ -1,20 +1,36 @@
 package com.mercadolivro.controllers
 
+import com.fasterxml.jackson.databind.util.BeanUtil
 import com.mercadolivro.controllers.request.PostCustomerRequest
 import com.mercadolivro.models.CustomerModel
+import org.springframework.beans.BeanUtils
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("customer")
 class CustomerController {
 
+    //"Mockando" um objeto
+    val customers = mutableListOf<CustomerModel>()
+
     @GetMapping
-    fun listCustomer(): CustomerModel {
-        return CustomerModel(1,"Fernando", "luiz123jfmg@gmail.com")
+    fun listCustomer(): MutableList<CustomerModel> {
+        return customers
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: PostCustomerRequest){
+
+        val id = if(customers.isEmpty()){
+            1
+        }else{
+            customers.last().id + 1;
+        }
+
+        customers.add(CustomerModel(id, customer.name, customer.email))
+
         println(customer)
     }
 
