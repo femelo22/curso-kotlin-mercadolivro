ca87a1a7bc1080dc8750907e3505d6d3a4d51821
luiz fernando <luiz123jfmg@gmail.com>
2022-01-10 08:56:29 -0300

    Criando nosso DataClass de customer e rota para listar

--- src/main/kotlin/com/mercadolivro/controllers/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controllers/CustomerController.kt
@@ -1,5 +1,6 @@
 package com.mercadolivro.controllers
 
+import com.mercadolivro.models.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 class CustomerController {
 
     @GetMapping
-    fun Hello(): String{
-        return "Primeiro endpoint em kotlin =)"
+    fun listCustomer(): CustomerModel {
+        return CustomerModel(1,"Fernando", "luiz123jfmg@gmail.com")
     }
 }
---
+++ src/main/kotlin/com/mercadolivro/models/CustomerModel.kt
@@ -0,0 +1,9 @@
+package com.mercadolivro.models
+
+data class CustomerModel (
+    var id: Int,
+
+    var name: String,
+
+    var email: String
+)
