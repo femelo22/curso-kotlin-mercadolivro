339599e91f67c533007cd80c6d32ad323eeb9d18
luiz fernando <luiz123jfmg@gmail.com>
2022-01-13 07:03:44 -0300

    Mapeando customer to entity, correção conexão com banco

--- src/main/kotlin/com/mercadolivro/models/CustomerModel.kt
+++ src/main/kotlin/com/mercadolivro/models/CustomerModel.kt
@@ -1,9 +1,17 @@
 package com.mercadolivro.models
 
+import javax.persistence.*
+
+@Entity(name = "customer")
 data class CustomerModel (
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Int,
 
+    @Column
     var name: String,
 
+    @Column
     var email: String
 )
