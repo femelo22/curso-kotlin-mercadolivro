5005f20ab1ae51a0d8e7e707ddf8e95f67f95482
luiz fernando <luiz123jfmg@gmail.com>
2022-01-17 12:43:26 -0300

    Rota salvar livro

---
+++ src/main/kotlin/com/mercadolivro/repositories/BookRepository.kt
@@ -0,0 +1,9 @@
+package com.mercadolivro.repositories
+
+import com.mercadolivro.models.BookModel
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface BookRepository: JpaRepository<BookModel, Int> {
+}
--- src/main/kotlin/com/mercadolivro/services/BookService.kt
+++ src/main/kotlin/com/mercadolivro/services/BookService.kt
@@ -1,12 +1,17 @@
 package com.mercadolivro.services
 
 import com.mercadolivro.models.BookModel
+import com.mercadolivro.repositories.BookRepository
 import org.springframework.stereotype.Service
 
 @Service
-class BookService {
+class BookService(
+    val bookRepository: BookRepository
+){
 
     fun create(book: BookModel) {
+
+        bookRepository.save(book)
 
     }
 }
