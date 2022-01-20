a3bc733690d9af9c97446779192b243c6e4baf90
luiz fernando <luiz123jfmg@gmail.com>
2022-01-20 07:59:39 -0300

    Criando nosso Controller Advice

--- src/main/kotlin/com/mercadolivro/controllers/response/ErrorResponse.kt
+++ src/main/kotlin/com/mercadolivro/controllers/response/ErrorResponse.kt
@@ -4,6 +4,6 @@ data class ErrorResponse (
     var httpCode: Int,
     var message: String,
     var internalCode: String,
-    var erros: List<FieldErrorResponse>
+    var erros: List<FieldErrorResponse>?
 
 )
