ec804cf8c843f260c386f4b7e8c8c230a85b08d9
luiz fernando <luiz123jfmg@gmail.com>
2022-01-21 07:34:51 -0300

    Colocando validações nos campos de customer

--- src/main/kotlin/com/mercadolivro/controllers/request/PostCustomerRequest.kt
+++ src/main/kotlin/com/mercadolivro/controllers/request/PostCustomerRequest.kt
@@ -1,11 +1,14 @@
 package com.mercadolivro.controllers.request
 
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotEmpty
+
 data class PostCustomerRequest (
 
-    var id: Int,
-
+    @field:NotEmpty(message = "Nome deve ser informado")
     var name: String,
 
+    @field:Email(message = "Email não é valido")
     var email: String
 
 )
--- src/main/kotlin/com/mercadolivro/enuns/Errors.kt
+++ src/main/kotlin/com/mercadolivro/enuns/Errors.kt
@@ -4,6 +4,7 @@ enum class Errors(val code: String, val message: String) {
 
     ML001("ML-001", "Book [%s] not exists"),
     ML002("ML-002", "Customer [%s] not exists"),
-    ML003("ML-003", "Cannot update book with status [%s]")
+    ML003("ML-003", "Cannot update book with status [%s]"),
+    ML004("ML-004", "Invalid Request")
 
 }
