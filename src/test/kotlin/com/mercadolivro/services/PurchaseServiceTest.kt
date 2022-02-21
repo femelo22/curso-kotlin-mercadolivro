bbde2fcd0866b37e6c452c3f1d5a36e55ec8d956
luiz fernando <luiz123jfmg@gmail.com>
2022-02-21 18:24:08 -0300

    test: create purchase and publish event

--- src/test/kotlin/com/mercadolivro/services/PurchaseServiceTest.kt
+++ src/test/kotlin/com/mercadolivro/services/PurchaseServiceTest.kt
@@ -1,11 +1,16 @@
 package com.mercadolivro.services
 
+import com.mercadolivro.events.PurchaseEvent
+import com.mercadolivro.helper.buildPurchase
 import com.mercadolivro.models.PurchaseModel
 import com.mercadolivro.repositories.PurchaseRepository
+import io.mockk.*
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.SpyK
 import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.context.ApplicationEventPublisher
 
@@ -22,6 +27,24 @@ class PurchaseServiceTest{
     @SpyK
     private lateinit var purchaseService: PurchaseService
 
+    val purchaseEventSlot = slot<PurchaseEvent>()
 
+
+    @Test
+    fun `should create purchase and publish event`() {
+        val purchaseFake = buildPurchase()
+
+        every { purchaseRepository.save(purchaseFake) } returns purchaseFake
+
+        every { applicationEventPublisher.publishEvent(any()) } just runs
+
+        purchaseService.create(purchaseFake)
+
+        verify(exactly = 1) { purchaseRepository.save(purchaseFake) }
+
+        verify { applicationEventPublisher.publishEvent(capture(purchaseEventSlot)) }
+
+        Assertions.assertEquals(purchaseFake, purchaseEventSlot.captured.purchaseModel)
+    }
 
 }
